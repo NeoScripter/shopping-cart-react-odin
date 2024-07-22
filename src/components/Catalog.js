c510ae9c94c6be3caf9d1db47dada954b3016926
Ilya <sange0337@gmail.com>
2024-07-22 16:13:23 +0800

    Added the load more button

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect  } from "react";
 import Item from "./Item";
 import { useCart } from "../hooks/CartContext";
 
 function Catalog() {
     const { cart, updateItemQuantity } = useCart();
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [itemsToShow, setItemsToShow] = useState(2);
+
+    const loadMoreItems = () => {
+        setItemsToShow((prevItemsToShow) => prevItemsToShow + 2);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+            setScreenWidth(width);
+            width < 540 ? setItemsToShow(2) : setItemsToShow(20);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
 
     return (
         <div>
@@ -11,7 +29,7 @@ function Catalog() {
                 Desserts
             </h1>
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {cart.map((dessert) => (
+                {cart.slice(0, itemsToShow).map((dessert) => (
                     <Item
                         key={dessert.id}
                         id={dessert.id}
@@ -24,6 +42,14 @@ function Catalog() {
                     />
                 ))}
             </div>
+            {itemsToShow < cart.length && screenWidth <= 640 && (
+                <button
+                    onClick={loadMoreItems}
+                    className="flex justify-between items-center w-max h-4 md:h-8 aspect-ratio-4/1 p-6 mx-auto rounded-full border border-red-800 gap-8 bg-color-orange text-white font-bold border-0 mb-8"
+                >
+                    Load More
+                </button>
+            )}
         </div>
     );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -34,18 +34,18 @@ function AddItem({ onClick }) {
   return (
     <div onClick={onClick} className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 flex justify-between items-center w-max h-4 md:h-8 bg-white p-6 rounded-full border border-gray-400 gap-4 cursor-pointer">
       <img src={addToCart} alt="Add to cart" className='h-6'/>
-      <div className='font-bold text-base'>Add to cart</div>
+      <div className='font-bold text-base select-none'>Add to cart</div>
     </div>
   );
 }
 
 function ChangeQuantity({ quantity, onIncrement, onDecrement }) {
   return (
-    <div className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 flex justify-between items-center w-max h-4 md:h-8 aspect-ratio-4/1 p-6 rounded-full border border-red-800 gap-8 bg-color-orange">
+    <div className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 flex justify-between items-center w-max h-4 md:h-8 cursor-pointer p-6 rounded-full border border-red-800 gap-8 bg-color-orange">
       <div onClick={onDecrement} className='flex justify-center items-center h-6 w-6 rounded-full border-2 border-white cursor-pointer'>
         <img src={decrement} alt="Decrement quantity" className='h-3 w-3'/>
       </div>
-      <div className='font-bold text-base text-white'>{quantity}</div>
+      <div className='font-bold text-base text-white select-none'>{quantity}</div>
       <div onClick={onIncrement} className='flex justify-center items-center h-6 w-6 rounded-full border-2 border-white cursor-pointer'>
         <img src={increment} alt="Increment quantity" className='h-3 w-3'/>
       </div>
