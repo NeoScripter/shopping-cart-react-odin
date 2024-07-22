590788ce531be0dbdf093a02aed48afd8783db86
Ilya <sange0337@gmail.com>
2024-07-22 17:04:15 +0800

    Changed the mouseenter event on the button

--- src/components/Item.js
+++ src/components/Item.js
@@ -25,9 +25,9 @@ function Item({ id, image, title, description, price, qnt, updateItemQuantity })
 
   return (
     <div className='mb-2'>
-      <div onMouseLeave={hideAddItems} onMouseEnter={displayAddItems} className='relative mb-8 imageHolder'>
+      <div className='relative mb-8 imageHolder'>
         <img src={image} alt={title} className={`object-center object-cover rounded-lg h-full w-full${itemExtraClass}`}/>
-        {!showAddItem && <AddItem />}
+        {!showAddItem && <AddItem onMouseEnter={displayAddItems}/>}
         {showAddItem && <ChangeQuantity quantity={qnt} onIncrement={handleIncrement} onDecrement={handleDecrement} onMouseLeave={hideAddItems}/>}
       </div>
       <p className='text-base text-gray-500'>{title}</p>
@@ -37,18 +37,18 @@ function Item({ id, image, title, description, price, qnt, updateItemQuantity })
   );
 }
 
-function AddItem() {
+function AddItem({ onMouseEnter }) {
   return (
-    <div className="transition-opacity absolute -bottom-6 left-1/2 transform -translate-x-1/2 flex justify-between items-center w-max h-4 md:h-8 bg-white p-6 rounded-full border border-gray-400 gap-4 cursor-pointer">
+    <div onMouseEnter={onMouseEnter} className="transition-opacity absolute -bottom-6 left-1/2 transform -translate-x-1/2 flex justify-between items-center w-max h-4 md:h-8 bg-white p-6 rounded-full border border-gray-400 gap-4 cursor-pointer">
       <img src={addToCart} alt="Add to cart" className='h-6'/>
       <div className='font-bold text-base select-none'>Add to cart</div>
     </div>
   );
 }
 
-function ChangeQuantity({ quantity, onIncrement, onDecrement }) {
+function ChangeQuantity({ quantity, onIncrement, onDecrement, onMouseLeave }) {
   return (
-    <div className="transition-opacity absolute z-10 -bottom-6 left-1/2 transform -translate-x-1/2 flex justify-between items-center w-max h-4 md:h-8 p-6 rounded-full border border-red-800 gap-8 bg-color-orange">
+    <div onMouseLeave={onMouseLeave} className="transition-opacity absolute z-10 -bottom-6 left-1/2 transform -translate-x-1/2 flex justify-between items-center w-max h-4 md:h-8 p-6 rounded-full border border-red-800 gap-8 bg-color-orange">
       <div onClick={onDecrement} className='flex justify-center items-center h-6 w-6 rounded-full border-2 border-white cursor-pointer'>
         <img src={decrement} alt="Decrement quantity" className='h-3 w-3'/>
       </div>
